6a225d5e8f631e17daa5802cb9de4ac6e368ccc2
unknown <osamadrbas31@gmail.com>
2024-01-06 22:01:41 +0200

    new

--- src/components/Header/HeaderButton.jsx
+++ src/components/Header/HeaderButton.jsx
@@ -36,7 +36,7 @@ function HeaderButton({ p }) {
     return (
         <div className="w-full h-screen  items-center flex justify-center absolute">
             <div  ref={projectButton} className=" w-[22%] pb-[22%] md:w-[10%] md:pb-[10%] translate-y-[120%] transition-all rounded-full duration-300 -z-20 relative  overflow-hidden  ">
-                <label ref={projectButtonText} className="w-full h-full absolute flex md:translate-y-[100%] items-center transition-all duration-300 justify-center text-[100%]  rounded-full text-theme">projects</label>
+                <label ref={projectButtonText} className="w-full h-full absolute flex translate-y-[100%] items-center transition-all duration-300 justify-center text-[100%]  rounded-full text-theme">projects</label>
                 <svg className=' absolute ' viewBox="0 0 140 140" preserveAspectRatio="xMinYMin meet">
                         <circle className=' transition-all ease-linear duration-[300ms]' ref={circleSvg} cx="50%" cy="50%" r="69" strokeDashoffset="432.8393859863281" strokeDasharray="432.8393859863281" stroke="#0E8388" strokeWidth="1" fill="none" />
                 </svg>    
