f9cdb9d2b65101679ecbc3c6a034d38497fee8f1
unknown <osamadrbas31@gmail.com>
2024-01-11 15:36:51 +0200

    new

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -7,7 +7,7 @@ function Project({project}) {
   },[v])
   return (
         <div className=' relative w-full flex flex-col'>
-          <Link to={project.link} className='w-full pb-[55%] relative overflow-hidden'>
+          <Link to={project.link} className='w-full outline-none pb-[55%] relative overflow-hidden'>
             <img className='w-full min-h-full transition-all duration-200 absolute' src={project.img}  alt="" />
             <video onMouseOver={()=>{
                 v.current.play()
@@ -17,7 +17,7 @@ function Project({project}) {
                 v.current.pause()
                 v.current.style.opacity="0%"
               }}
-              ref={v} className='min-w-full w-[200%] min-h-full absolute opacity-0 transition-all object-fill ' src={project.video}  type='video/mp4'  />
+              ref={v} className='min-w-[130%]  absolute opacity-0 transition-all ' src={project.video}  type='video/mp4'  />
           </Link>  
             <div className="flex flex-row text-white capitalize py-[3%] px-[2%] bg-dark-1 border-b-[1px] border-b-theme">{project.name}</div>
         </div>
