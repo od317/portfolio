2df41a0bba088a48e2d0f17df91364e1bd8d6409
unknown <osamadrbas31@gmail.com>
2024-01-11 15:29:13 +0200

    new

--- src/components/Header/HeaderButton.jsx
+++ src/components/Header/HeaderButton.jsx
@@ -38,7 +38,7 @@ function HeaderButton({ p }) {
             <div  ref={projectButton} className=" w-[22%] pb-[22%] md:w-[10%] md:pb-[10%] translate-y-[120%] transition-all rounded-full duration-300 -z-20 relative  overflow-hidden  ">
                 <label ref={projectButtonText} className="w-full h-full absolute flex translate-y-[100%] items-center transition-all duration-300 justify-center text-[100%]  rounded-full text-theme">projects</label>
                 <svg className=' absolute ' viewBox="0 0 140 140" preserveAspectRatio="xMinYMin meet">
-                        <circle className=' transition-all ease-linear duration-[300ms]' ref={circleSvg} cx="50%" cy="50%" r="69" strokeDashoffset="432.8393859863281" strokeDasharray="432.8393859863281" stroke="#0079FF" strokeWidth="1" fill="none" />
+                        <circle className=' transition-all ease-linear duration-[300ms]' ref={circleSvg} cx="50%" cy="50%" r="69" strokeDashoffset="432.8393859863281" strokeDasharray="432.8393859863281" stroke="#00ADB5" strokeWidth="1" fill="none" />
                 </svg>    
             </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ export default {
        'dark':{
         1:'#121212'
        },
-       'theme':'#00B8A9',
+       'theme':'#00ADB5',
        'theme2':'#FF004D'
     },
     extend: {
